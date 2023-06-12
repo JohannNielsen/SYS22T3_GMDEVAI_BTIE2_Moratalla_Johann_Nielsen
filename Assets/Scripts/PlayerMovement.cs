c0152e47484b2aa0d35fa944175b54a910edde71
JohannNielsen <133647351+JohannNielsen@users.noreply.github.com>
2023-06-13 03:31:17 +0800

    Add files via upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+    public float lookSensitivity = 5.0f;
+    public float maxLookUpAngle = 80.0f;
+    public float maxLookDownAngle = -80.0f;
+
+    private Rigidbody rb;
+    private Camera playerCam;
+    private float xRotation = 0.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerCam = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Handle player movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput) * moveSpeed;
+        movement = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0) * movement;
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+
+        // Handle player looking
+        float mouseX = Input.GetAxis("Mouse X") * lookSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * lookSensitivity;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, maxLookDownAngle, maxLookUpAngle);
+        playerCam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/TraveltoGoal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraveltoGoal : MonoBehaviour
+{
+    public Transform goal;
+    public float acceleration = 3f;
+    public float deceleration = 3f;
+    public float maxSpeed = 5f;
+    
+    private float currentSpeed = 0f;
+
+    private void Update()
+    {
+        Vector3 lookAtGoal = new Vector3(goal.position.x, transform.position.y, goal.position.z);
+        Vector3 direction = lookAtGoal - transform.position;
+
+        transform.rotation = Quaternion.LookRotation(direction);
+
+        float distanceToGoal = Vector3.Distance(transform.position, lookAtGoal);
+
+        // Calculate target speed based on the distance to the goal
+        float targetSpeed = Mathf.Clamp(distanceToGoal, 0f, maxSpeed);
+
+        // Smoothly adjust the current speed using Lerp
+        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, Time.deltaTime * acceleration);
+
+        // Move the object forward based on the current speed
+        if (Vector3.Distance(lookAtGoal, transform.position) > 1)
+        {
+            transform.Translate(0f, 0f, currentSpeed * Time.deltaTime);
+        }
+            
+    }
+}
